abfb7b8012fb86b3f12bb274f8b40dca3682453c
Heegyeong Ryu <rhk3562@naver.com>
2021-06-18 21:50:10 +0900

    자바스크립트 표현

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 function App() {
+  // 기본적으로 const 사용. 변경해야 할 때 let 쓰기.
+  const name = '리액트';
   return (
-    // Fragment 사용
     <>
-      <h1>리액트 안녕!</h1>
+      <h1>{name} 안녕!</h1>
       <h2>잘 작동하니?</h2>
     </>
   );
